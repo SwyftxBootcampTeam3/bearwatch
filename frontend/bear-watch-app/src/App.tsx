c7dffa22fef4e38f4f0f313226c7edcb514d291b
Rachel Chiong <42544664+RachelChiong@users.noreply.github.com>
2022-06-29 10:15:41 +1000

    Login/Signup page

--- frontend/bear-watch-app/src/App.tsx
+++ frontend/bear-watch-app/src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, AppBar, Container, Toolbar } from '@mui/material';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
     <div className="App">
     <Header title='BearWatch' subtitle='A subtitle' user='TestUser'/>
-    
+    {!isSignedIn && <LoginPage /> }
     </div>
   );
 }
