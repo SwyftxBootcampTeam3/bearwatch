6c492cbbb368d40219c89cbd103968f703bc635c
Stephan.A <stephan.a@192-168-1-123.tpgi.com.au>
2022-07-06 14:25:50 +1000

    clery boilerplate

--- backend/app/services/celery_worker.py
+++ backend/app/services/celery_worker.py
@@ -12,3 +12,22 @@ celery.conf.result_backend = config("CELERY_RESULT_BACKEND", cast=str)
 def create_task(a,b,c):
     time.sleep(a)
     return b + c
+
+
+# CronJob 1 - Fetch all updated asset prices
+@celery.task(name="fetch-updated-assets")
+def fetch_updated_assets():
+    # Fetch from swftx api
+    # Iterate over all json objects and for each
+    # insert_into_db
+
+
+def insert_into_db(name, code, last_price):
+    # Write query to insert new prices into assets
+
+
+# CronJob 2 - Fetch all triggered alerts
+@celery.task(name="get-triggered-alerts")
+def get_triggered_alerts():
+    # Fetch from DB (write query)
+    # Iterate over and send alerts using lanas stuff
